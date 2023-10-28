80da23f2f7d4c2c00aff0addf40feea612441bf7
Nguyen-Hust <68378378+Nguyen-Hust@users.noreply.github.com>
2023-10-28 14:06:25 +0700

    commit

---
+++ questions.js
@@ -0,0 +1,66 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question:
+      "UNESCO ra Nghị quyết 24C/18.65 tôn vinh Chủ tịch Hồ Chí Minh là Anh hùng giải phóng dân tộc và danh nhân văn hóa kiệt xuất vào năm:",
+    answer: "Năm 1987",
+    options: ["Năm 1969", "Năm 1975", "Năm 1987", "Năm 1990"],
+  },
+  {
+    numb: 2,
+    question: "Một trong những nguồn gốc của tư tưởng Hồ Chí Minh là tiếp thu:",
+    answer: "Giá trị truyền thống dân tộc",
+    options: [
+      "Lòng nhân ái, đức hy sinh của Hồi giáo.",
+      "Các giá trị văn hóa tiến bộ của thời kỳ Phục hưng",
+      "Giá trị truyền thống dân tộc",
+      "Văn hóa Phục hưng",
+    ],
+  },
+  {
+    numb: 3,
+    question:
+      '"Dân tộc ta, nhân dân ta, non sông đất nước ta đã sinh ra Hồ Chủ tịch, Người anh hùng dân tộc vĩ đại, và chính Người đã làm rạng rỡ dân tộc ta, nhân dân ta và non sông đất nước ta". Đó là khẳng định của:',
+    answer: "Ban Chấp hành Trung ương Đảng Lao động Việt Nam",
+    options: [
+      "Đồng chí Võ Nguyên Giáp",
+      "Đồng chí Trường Chinh",
+      "Đồng chí Phạm Văn Đồng",
+      "Ban Chấp hành Trung ương Đảng Lao động Việt Nam",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "“Trong lúc này, thời cơ thuận lợi đã tới. Dù hy sinh tới đâu, dù phải đốt cháy cả dãy Trường Sơn cũng phải kiên quyết giành cho được độc lập”. Hồ Chí Minh đã chỉ thị điều đó cho ai?",
+    answer: "Võ Nguyên Giáp",
+    options: ["Võ Nguyên Giáp", "Trường Chinh", "Phạm Văn Đồng", "Vũ Kỳ"],
+  },
+  {
+    numb: 5,
+    question:
+      'Trong Tuyên ngôn độc lập, Hồ Chí Minh đã khẳng định quyết tâm của nhân dân ta: "Toàn thể dân tộc Việt Nam quyết đem (.....) để giữ vững quyền tự do, độc lập ấy". Điền từ vào chỗ trống:',
+    answer: "Tất cả tinh thần và lực lượng, tính mạng và của cải",
+    options: [
+      "Toàn bộ sức lực",
+      "Tất cả tinh thần và lực lượng",
+      "Tính mạng và của cải",
+      "Tất cả tinh thần và lực lượng, tính mạng và của cải",
+    ],
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
--- script.js
+++ script.js
@@ -5,25 +5,27 @@ const stopButton = document.getElementById("stop");
 const gameContainer = document.querySelector(".game-container");
 const result = document.getElementById("result");
 const controls = document.querySelector(".controls-container");
+const quiz_box = document.querySelector(".quiz_box");
+const option_list = document.querySelector(".option_list");
+const time_line = document.querySelector("header .time_line");
+const timeCount = document.querySelector(".timer .timer_sec");
+const timeText = document.querySelector(".timer .time_left_txt");
+const next_btn = document.querySelector("footer .next_btn");
+const bottom_ques_counter = document.querySelector("footer .total_que");
+const shadow = document.querySelector(".shadow");
 let cards;
 let interval;
 let firstCard = false;
 let secondCard = false;
+let que_count = 0;
+let count_wrong = 0;
+let tickIconTag = '<div class="icon tick"><i class="fas fa-check"></i></div>';
+let crossIconTag = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+
+let question_random = [];
 
 //Items array
 const items = [
-  // { name: "bee", image: "bee.png" },
-  // { name: "crocodile", image: "crocodile.png" },
-  // { name: "macaw", image: "macaw.png" },
-  // { name: "gorilla", image: "gorilla.png" },
-  // { name: "tiger", image: "tiger.png" },
-  // { name: "monkey", image: "monkey.png" },
-  // { name: "chameleon", image: "chameleon.png" },
-  // { name: "piranha", image: "piranha.png" },
-  // { name: "anaconda", image: "anaconda.png" },
-  // { name: "sloth", image: "sloth.png" },
-  // { name: "cockatoo", image: "cockatoo.png" },
-  // { name: "toucan", image: "toucan.png" },
   { name: "image1", image: "image1.jpg" },
   { name: "image2", image: "image2.jpg" },
   { name: "image3", image: "image3.jpg" },
@@ -31,7 +33,7 @@ const items = [
   { name: "image5", image: "image5.jpg" },
   { name: "image6", image: "image6.jpg" },
   { name: "image7", image: "image7.jpg" },
-  { name: "image8", image: "image8.jpg" },
+  { name: "bingo", image: "bingo.png" },
 ];
 
 //Initial Time
@@ -106,10 +108,21 @@ const matrixGenerator = (cardValues, size = 4) => {
   cards = document.querySelectorAll(".card-container");
   cards.forEach((card) => {
     card.addEventListener("click", () => {
+      let value = card.getAttribute("data-card-value");
       //If selected card is not matched yet then only run (i.e already matched card when clicked would be ignored)
       if (!card.classList.contains("matched")) {
         //flip the cliked card
         card.classList.add("flipped");
+        if (value == "bingo") {
+          card.classList.add("matched");
+          count_wrong = 0;
+          quiz_box.classList.add("activeQuiz");
+          shadow.classList.add("on");
+          showQuetions(que_count);
+          startTimer(15);
+          startTimerLine(0);
+          return;
+        }
         //if it is the firstcard (!firstCard since firstCard is initially false)
         if (!firstCard) {
           //so current card will become firstCard
@@ -134,7 +147,7 @@ const matrixGenerator = (cardValues, size = 4) => {
 
             if (winCount == Math.floor(cardValues.length / 2)) {
               setTimeout(() => {
-                result.innerHTML = `<h2>You Won</h2>
+                result.innerHTML = `<h2>Bạn thắng</h2>
                 <h4>Moves: ${movesCount}</h4>`;
                 stopGame();
               }, 1500);
@@ -162,6 +175,7 @@ startButton.addEventListener("click", () => {
   seconds = 0;
   minutes = 0;
   //controls amd buttons visibility
+  question_random = shuffle(questions);
   controls.classList.add("hide");
   stopButton.classList.remove("hide");
   startButton.classList.add("hide");
@@ -176,6 +190,12 @@ startButton.addEventListener("click", () => {
 stopButton.addEventListener(
   "click",
   (stopGame = () => {
+    que_count = 0;
+    count_wrong = 0;
+    bottom_ques_counter.innerHTML = "";
+    quiz_box.classList.remove("activeQuiz");
+    shadow.classList.remove("on");
+    next_btn.classList.remove("show");
     controls.classList.remove("hide");
     stopButton.classList.add("hide");
     startButton.classList.remove("hide");
@@ -188,6 +208,163 @@ const initializer = () => {
   result.innerText = "";
   winCount = 0;
   let cardValues = generateRandom();
-  console.log(cardValues);
+
   matrixGenerator(cardValues);
 };
+
+function showQuetions(index) {
+  const que_text = document.querySelector(".que_text");
+  //creating a new span and div tag for question and option and passing the value using array index
+  let que_tag =
+    "<span>" +
+    (que_count + 1) +
+    ". " +
+    question_random[index].question +
+    "</span>";
+  let option_tag =
+    '<div class="option"><span>' +
+    question_random[index].options[0] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    question_random[index].options[1] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    question_random[index].options[2] +
+    "</span></div>" +
+    '<div class="option"><span>' +
+    question_random[index].options[3] +
+    "</span></div>";
+  que_text.innerHTML = que_tag; //adding new span tag inside que_tag
+  option_list.innerHTML = option_tag; //adding new div tag inside option_tag
+
+  const option = option_list.querySelectorAll(".option");
+
+  // set onclick attribute to all available options
+  for (i = 0; i < option.length; i++) {
+    option[i].setAttribute("onclick", "optionSelected(this)");
+  }
+}
+
+function startTimer(time) {
+  counter = setInterval(timer, 1000);
+  function timer() {
+    timeCount.textContent = time; //changing the value of timeCount with time value
+    time--; //decrement the time value
+    if (time < 9) {
+      //if timer is less than 9
+      let addZero = timeCount.textContent;
+      timeCount.textContent = "0" + addZero; //add a 0 before time value
+    }
+    if (time < 0) {
+      //if timer is less than 0
+      clearInterval(counter); //clear counter
+      count_wrong += 1;
+      if (count_wrong == 2) {
+        bottom_ques_counter.innerHTML = "Bạn đã thua";
+        next_btn.classList.add("show");
+      } else {
+        setTimeout(() => {
+          que_count += 1;
+          showQuetions(que_count);
+          startTimer(15);
+          startTimerLine(0);
+        }, 1000);
+      }
+      timeText.textContent = "Time Off"; //change the time text to time off
+      const allOptions = option_list.children.length; //getting all option items
+      let correcAns = question_random[que_count].answer; //getting correct answer from array
+      for (i = 0; i < allOptions; i++) {
+        if (option_list.children[i].textContent == correcAns) {
+          //if there is an option which is matched to an array answer
+          option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+          option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+        }
+      }
+      for (i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+      }
+    }
+  }
+}
+
+function startTimerLine(time) {
+  counterLine = setInterval(timer, 29);
+  function timer() {
+    time += 1; //upgrading time value with 1
+    time_line.style.width = time + "px"; //increasing width of time_line with px by time value
+    if (time > 549) {
+      //if time value is greater than 549
+      clearInterval(counterLine); //clear counterLine
+    }
+  }
+}
+
+function optionSelected(answer) {
+  clearInterval(counter); //clear counter
+  clearInterval(counterLine); //clear counterLine
+  let userAns = answer.textContent; //getting user selected option
+  let correcAns = question_random[que_count].answer; //getting correct answer from array
+  const allOptions = option_list.children.length; //getting all option items
+
+  if (userAns == correcAns) {
+    //if user selected option is equal to array's correct answer
+    answer.classList.add("correct"); //adding green color to correct selected option
+    answer.insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to correct selected option
+    winCount += 0.5;
+    que_count += 1;
+    setTimeout(() => {
+      quiz_box.classList.remove("activeQuiz");
+      shadow.classList.remove("on");
+    }, 1000);
+  } else {
+    count_wrong += 1;
+    if (count_wrong == 2) {
+      bottom_ques_counter.innerHTML = "Bạn đã thua";
+      next_btn.classList.add("show");
+    } else {
+      setTimeout(() => {
+        que_count += 1;
+        showQuetions(que_count);
+        startTimer(15);
+        startTimerLine(0);
+      }, 1000);
+    }
+    answer.classList.add("incorrect"); //adding red color to correct selected option
+    answer.insertAdjacentHTML("beforeend", crossIconTag); //adding cross icon to correct selected option
+
+    for (i = 0; i < allOptions; i++) {
+      if (option_list.children[i].textContent == correcAns) {
+        //if there is an option which is matched to an array answer
+        option_list.children[i].setAttribute("class", "option correct"); //adding green color to matched option
+        option_list.children[i].insertAdjacentHTML("beforeend", tickIconTag); //adding tick icon to matched option
+      }
+    }
+  }
+  for (i = 0; i < allOptions; i++) {
+    option_list.children[i].classList.add("disabled"); //once user select an option then disabled all options
+  }
+}
+
+next_btn.onclick = () => {
+  stopGame();
+};
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex > 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+}
